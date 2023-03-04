904cbcddc0868556a24c8fa08d4f1b15ca010d02
4za4s <arbo3802@gmail.com>
2023-03-04 17:12:30 +1100

    something, search bar exists but there is an error

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -9,4 +9,17 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+
+    public function search(Request $request)
+    {
+        if ($request -> search)
+        {
+            $search = User::where('name', 'like', '%' . $request -> search . '%') -> get();
+            return view('welcome')->with($search);
+        }
+        else
+        {
+            return view('welcome');
+        }
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\User;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::controller(App\Http\Controllers\Controller::class)->group( function () {
+
+    Route::get('/', function () {
+        if (request()->search) {
+            $search = User::where('name', 'like', '%' . request()->search . '%')->get();
+            return view('welcome')->with($search);
+        }
+        else {
+            return view('welcome');
+        }
+    });
 });
